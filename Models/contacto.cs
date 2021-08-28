ac43e1d51ba5818bff8d7de51c1e785c3f4afe0d
Danny Flores <floresfredj@gmail.com>
2021-08-28 03:01:17 -0500

    .

--- Models/contacto.cs
+++ Models/contacto.cs
@@ -2,6 +2,8 @@ namespace demomvc.Models
 {
     public class contacto
     {
-        
-    }
+        public string Correo {get; set;}
+        public string Telefono {get; set;}
+        public string Nombres {get; set;}
+        public string Comentarios {get; set;}
 }
